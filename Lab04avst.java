a6ebac85347e178afdf191d821dc604f83b690d2
13180 <w@VTSD.COM>
2022-10-12 07:18:56 -0400

    dcrfdcghv

--- Lab04avst.java
+++ Lab04avst.java
@@ -12,12 +12,23 @@ public class Lab04avst
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        double numMonths = numYears * 12;
+        // Convert to decimal, then months
+        double monthlyRate = (annualRate / 100) / 12;
 
+        double num = monthlyRate * Math.pow(1 + monthlyRate, numMonths);
+        double denom = Math.pow(1 + monthlyRate, numMonths) - 1;
+        double monPay = (num/denom) * principal;
 
+        double totPay = monPay * numMonths;
+        double totInt = totPay - principal;
 
-
-
-
+        System.out.println("Principal:          $" + principal);
+        System.out.println("Annual Rate:        " + annualRate + "%");
+        System.out.println("Number of Years:    " + numYears);
+        System.out.println("Monthly Payment:    $" + monPay);
+        System.out.println("Total Payments:     $" + totPay);
+        System.out.println("Total Payments:     $" + totInt);
 
 
 
